3d4c88666b5553b553323d3be18fe1a2da8ba6b7
Marco <marco.riopel@hotmail.com>
2019-03-13 23:23:19 -0400

    Fini de reviser le code du son

--- codeCommun/Librairie/sources/son.cpp
+++ codeCommun/Librairie/sources/son.cpp
@@ -12,58 +12,23 @@
 
 void allumerSon(int position)
 {
-    uint8_t premierePosition = 45;
+    uint8_t premierePosition = 45; //Premiere position donnee pour les notes MIDI
     
-    static uint16_t frequences[] = {110,
-                                    117,
-                                    123,
-                                    131,
-                                    139,
-                                    147,
-                                    156,
-                                    165,
-                                    175,
-                                    185,
-                                    196,
-                                    208,
-                                    220,
-                                    233,
-                                    247,
-                                    262,
-                                    277,
-                                    294,
-                                    311,
-                                    330,
-                                    349,
-                                    370,
-                                    392,
-                                    415,
-                                    440,
-                                    466,
-                                    494,
-                                    523,
-                                    554,
-                                    587,
-                                    622,
-                                    659,
-                                    698,
-                                    740,
-                                    784,
-                                    831,
-                                    880};
+    uint16_t frequences[] = {110, 117, 123, 131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 
+                                    392, 415, 440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880};
 
     uint8_t positionDansTableau = position - premierePosition;
 
     TCNT0 = 0; //On reset le compteur si le OCR0A est plus petit que le TCNT0
     TCCR0A |= (1 << COM0A0) | (1 << WGM01); //Active 0C0A quand on a un compare match
-    TCCR0B |= (1 << CS02);
+    TCCR0B |= (1 << CS02); //Prescaler de 256
 
-    static uint16_t compteur = F_CPU / 256;
+    uint16_t compteur = F_CPU / 256; //Comportement du compteur selon le prescaler de 256
 
     OCR0A = compteur / (frequences[positionDansTableau] * 2) - 1;
 }
 
 void arreterSon()
 {
-    TCCR0A &= ~(1 << COM0A1) & ~(1 << COM0A0);
+    TCCR0A &= ~(1 << COM0A1) & ~(1 << COM0A0); //On deconnece le OC0A pour un retour d'utilisation normal du port
 }
